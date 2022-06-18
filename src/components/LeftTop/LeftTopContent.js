69e5bcf66e12fee081231b3f1277e2de7302767d
smackere42 <foxpain12@gmail.com>
2022-06-18 22:55:16 +0300

    Links fix (target='_blank')

--- src/components/LeftTop/LeftTopContent.js
+++ src/components/LeftTop/LeftTopContent.js
@@ -3,7 +3,7 @@ import styles from'./leftTop.module.scss'
 const LeftTop = () => {
 return(
     <div id="leftTop" className='leftTop'>
-    <a tabIndex={0} className='drugoeDelo' href='https://drugoedelo.ru/' title='Другое дело' alt='Другое дело сылка'><div className='logoMap'></div></a>
+    <a tabIndex={0} className='drugoeDelo' href='https://drugoedelo.ru/' title='Другое дело' alt='Другое дело сылка' target="_blank"><div className='logoMap'></div></a>
     <div className='instruction'></div>
     </div>
 )};
